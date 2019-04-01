958b3d977e1897a176f4f6b8af6a0e47ac97c287
Kamran Ali <kamali@expedia.com>
2019-04-01 18:10:24 +0530

    Correcting data path

--- spark-scripts/src/main/scala/in/kamranali/customerOrder/PurchaseByCustomer.scala
+++ spark-scripts/src/main/scala/in/kamranali/customerOrder/PurchaseByCustomer.scala
@@ -14,14 +14,14 @@ object PurchaseByCustomer {
     // Read the data file
     // Loading data into RDD constant lines
     // CustomerID, ItemID, AmountSpentOnThatItem
-    val lines = sc.textFile("./src/main/resources/customer-orders.csv")
+    val lines = sc.textFile("./src/main/resources/customerdata/customer-orders.csv")
 
     val purchase = lines.map(lines => {
-      val fields = lines.split(",");
-      (fields(0).toInt, fields(2).toFloat);
+      val fields = lines.split(",")
+      (fields(0).toInt, fields(2).toFloat)
     })
 
-     val res = purchase.reduceByKey((x,y) => (x+y))
+     val res = purchase.reduceByKey((x,y) => x+y)
 
     // Invert Tuple to sort based on amount spend
     val invertedSort = res.map(x => (x._2, x._1)).sortByKey()
